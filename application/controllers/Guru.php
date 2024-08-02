4bbee60df9e0a47a7afb47e181a3c5a4cae0b7aa
erwnwin <erwin18@mhs.akba.ac.id>
2024-08-03 05:48:24 +0800

    Commit lagi

--- application/controllers/Guru.php
+++ application/controllers/Guru.php
@@ -41,6 +41,11 @@ class Guru extends CI_Controller
         $password = $this->input->post('password');
         $warna_guru = $this->input->post('warna_guru');
 
+        if (empty($nama) || empty($nip_nik) || empty($alamat) || empty($jenis_kelamin) || empty($telp_wa) || empty($alamat_email) || empty($password)) {
+            // Handle the error (e.g., set an error message and redirect)
+            return;
+        }
+
         $data = array(
             'nama' => $nama,
             'nip_nik' => $nip_nik,
@@ -50,17 +55,12 @@ class Guru extends CI_Controller
             'alamat_email' => $alamat_email,
             'password' => $password,
             'warna_guru' => $warna_guru,
-
         );
 
         $this->db->insert('guru', $data);
 
         $token = "R1bqBzS9scy-5+uYzRHq";
         $target = '62' . $telp_wa;
-
-        // $token = "r5-2#s!RR_b6umzdW4pm";
-        // $target = "6282194460105";
-
 
         $curl = curl_init();
 
@@ -73,39 +73,109 @@ class Guru extends CI_Controller
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => array(
+            CURLOPT_POSTFIELDS => http_build_query(array(
                 'target' => $target,
-                'message' => '*Hai, Bapak/Ibu ' . $this->input->post('nama') . '*
-                
-*Salam Sejahtera, Bapak/Ibu Guru SD*
-
-Data anda berhasil ditambahkan ke database sistem. Bapak/Ibu Guru dapat login ke sistem Penjadwalan Mata Pelajaran menggunakan user login di bawah ini:
-
-*Username/alamat email :* ' . $this->input->post('alamat_email') . '
-*Password :* ' . $this->input->post('password') . '
-
-Data akun login anda dapat anda update melalui menu profil ketika Bapak/Ibu login nantinya. Terima kasih.
-                
-Pesan ini dikirim secara otomatis oleh sistem',
-            ),
+                'message' => '*Hai, Bapak/Ibu ' . $nama . '* ...'
+            )),
             CURLOPT_HTTPHEADER => array(
-                "Authorization: $token" //change TOKEN to your actual token
+                "Authorization: $token"
             ),
         ));
 
         $response = curl_exec($curl);
 
+        if (curl_errno($curl)) {
+            echo 'Error:' . curl_error($curl);
+        }
+
         curl_close($curl);
-        echo $response;
 
         $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-        <h4><i class="icon fa fa-check"></i> Sukses</h4>
-        Data guru berhasil disimpan!
-        </div>');
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <h4><i class="icon fa fa-check"></i> Sukses</h4>
+    Data guru berhasil disimpan!
+    </div>');
 
         redirect(base_url('guru'));
     }
+
+
+    //     public function add_act()
+    //     {
+    //         $nama = $this->input->post('nama');
+    //         $nip_nik = $this->input->post('nip_nik');
+    //         $alamat = $this->input->post('alamat');
+    //         $jenis_kelamin = $this->input->post('jenis_kelamin');
+    //         $telp_wa = $this->input->post('telp_wa');
+    //         $alamat_email = $this->input->post('alamat_email');
+    //         $password = $this->input->post('password');
+    //         $warna_guru = $this->input->post('warna_guru');
+
+    //         $data = array(
+    //             'nama' => $nama,
+    //             'nip_nik' => $nip_nik,
+    //             'alamat' => $alamat,
+    //             'jenis_kelamin' => $jenis_kelamin,
+    //             'telp_wa' => '62' . $telp_wa,
+    //             'alamat_email' => $alamat_email,
+    //             'password' => $password,
+    //             'warna_guru' => $warna_guru,
+
+    //         );
+
+    //         $this->db->insert('guru', $data);
+
+    //         $token = "R1bqBzS9scy-5+uYzRHq";
+    //         $target = '62' . $telp_wa;
+
+    //         // $token = "r5-2#s!RR_b6umzdW4pm";
+    //         // $target = "6282194460105";
+
+
+    //         $curl = curl_init();
+
+    //         curl_setopt_array($curl, array(
+    //             CURLOPT_URL => 'https://api.fonnte.com/send',
+    //             CURLOPT_RETURNTRANSFER => true,
+    //             CURLOPT_ENCODING => '',
+    //             CURLOPT_MAXREDIRS => 10,
+    //             CURLOPT_TIMEOUT => 0,
+    //             CURLOPT_FOLLOWLOCATION => true,
+    //             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    //             CURLOPT_CUSTOMREQUEST => 'POST',
+    //             CURLOPT_POSTFIELDS => array(
+    //                 'target' => $target,
+    //                 'message' => '*Hai, Bapak/Ibu ' . $this->input->post('nama') . '*
+
+    // *Salam Sejahtera, Bapak/Ibu Guru SD*
+
+    // Data anda berhasil ditambahkan ke database sistem. Bapak/Ibu Guru dapat login ke sistem Penjadwalan Mata Pelajaran menggunakan user login di bawah ini:
+
+    // *Username/alamat email :* ' . $this->input->post('alamat_email') . '
+    // *Password :* ' . $this->input->post('password') . '
+
+    // Data akun login anda dapat anda update melalui menu profil ketika Bapak/Ibu login nantinya. Terima kasih.
+
+    // Pesan ini dikirim secara otomatis oleh sistem',
+    //             ),
+    //             CURLOPT_HTTPHEADER => array(
+    //                 "Authorization: $token" //change TOKEN to your actual token
+    //             ),
+    //         ));
+
+    //         $response = curl_exec($curl);
+
+    //         curl_close($curl);
+    //         echo $response;
+
+    //         $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible">
+    //         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    //         <h4><i class="icon fa fa-check"></i> Sukses</h4>
+    //         Data guru berhasil disimpan!
+    //         </div>');
+
+    //         redirect(base_url('guru'));
+    //     }
 
     public function edit_act()
     {
