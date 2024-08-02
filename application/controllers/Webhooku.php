06842d36ffcff70d1a3db52f5e11942c492fa5c0
erwnwin <erwin18@mhs.akba.ac.id>
2024-08-02 09:56:05 +0800

    Commit

--- application/controllers/Webhooku.php
+++ application/controllers/Webhooku.php
@@ -1,21 +1,112 @@
 <?php
+
 header('Content-Type: application/json; charset=utf-8');
 
 $json = file_get_contents('php://input');
 $data = json_decode($json, true);
+
+// Extract data from request
 $device = $data['device'];
 $sender = $data['sender'];
 $message = $data['message'];
-$member = $data['member']; //group member who send the message
-$name = $data['name'];
-$location = $data['location'];
-//data below will only received by device with all feature package
-//start
-$url =  $data['url'];
-$filename =  $data['filename'];
-$extension =  $data['extension'];
-//end
+$name = $data['name'] ?? '';
+$url = $data['url'] ?? '';
+$filename = $data['filename'] ?? '';
+$extension = $data['extension'] ?? '';
 
+// Database connection details
+$servername = "localhost";
+$database = "hvsteofj_jadwalta";
+$username = "hvsteofj_root";
+$password = "@Erwin2024#";
+
+// Create connection
+$koneksi = mysqli_connect($servername, $username, $password, $database);
+
+if (!$koneksi) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Fetch guru details based on sender's phone number
+$sql1 = "SELECT * FROM guru WHERE telp_wa='$sender'";
+$result1 = mysqli_query($koneksi, $sql1);
+
+if (mysqli_num_rows($result1) > 0) {
+    $row1 = mysqli_fetch_assoc($result1);
+    $telp_wa1 = $row1['telp_wa'];
+    $nama1 = $row1['nama'];
+    $alamat_email1 = $row1['alamat_email'];
+    $password1 = md5($row1['password']);
+} else {
+    $reply = [
+        "message" => "Maaf, data guru tidak ditemukan.\nPesan ini dikirim secara otomatis oleh sistem",
+    ];
+    sendFonnte($sender, $reply);
+    exit;
+}
+
+// Handle message based on the received command
+if ($message == "#jadwalsaya") {
+    // Fetch jadwal for the given guru
+    $sql2 = "SELECT * FROM penjadwalan 
+             INNER JOIN guru ON guru.id_guru = penjadwalan.id_guru 
+             WHERE guru.telp_wa='$telp_wa1'";
+    $result2 = mysqli_query($koneksi, $sql2);
+
+    if (mysqli_num_rows($result2) > 0) {
+        $jadwal_list = "";
+        while ($row2 = mysqli_fetch_assoc($result2)) {
+            $jadwal_list .= "Mata Pelajaran: " . $row2['mata_pelajaran'] . "\n";
+            $jadwal_list .= "Hari: " . $row2['hari'] . "\n";
+            $jadwal_list .= "Jam: " . $row2['jam'] . "\n\n";
+        }
+
+        $reply = [
+            "message" => "Jadwal Anda adalah:\n\n" . $jadwal_list .
+                "Pesan ini dikirim secara otomatis oleh sistem",
+        ];
+    } else {
+        $reply = [
+            "message" => "Tidak ada jadwal yang ditemukan untuk Anda.\n\nPesan ini dikirim secara otomatis oleh sistem",
+        ];
+    }
+} elseif ($message == "#akunsaya") {
+    if ($telp_wa1 == $sender) {
+        $reply = [
+            "message" => "*Hai " . $nama1 . "*\n\n" .
+                "Akun Login untuk Sistem Informasi Penjadwalan Mata Pelajaran (SiJadwalTa) adalah\n\n" .
+                "*Username/alamat email* = " . $alamat_email1 . "\n" .
+                "*Password* = r#h#si#\n\n" .
+                "Bapak/Ibu dapat mengubah detail akun login di menu Profil. Jika Bapak/Ibu Guru *Lupa Password* silahkan menghubungi admin SiJadwalTa. Terima Kasih\n\n" .
+                "Pesan ini dikirim secara otomatis oleh sistem",
+        ];
+    } else {
+        $reply = [
+            "message" => "Maaf tidak ditemukan data terkait.\n\nPesan ini dikirim secara otomatis oleh sistem",
+        ];
+    }
+} elseif ($message == "#nomorhpguru") {
+    $reply = [
+        "message" => "Nomor HP Guru dapat dilihat pada halaman utama SiJadwalTa pada menu Lainnya >> No. HP/WA Guru\n\n" .
+            "Pesan ini dikirim secara otomatis oleh sistem",
+    ];
+} else {
+    $reply = [
+        "message" => "Mohon Maaf, " . $nama1 . "\n\n" .
+            "BOT ini hanya membalas pesan anda dengan kata kunci:\n\n" .
+            "*#jadwalsaya* = untuk mengetahui jadwal mengajar guru\n" .
+            "*#akunsaya* = untuk mengetahui akun login guru\n" .
+            "*#nomorhpguru* = untuk mengetahui nomor hp/wa guru\n\n" .
+            "Pesan ini dikirim secara otomatis oleh sistem",
+    ];
+}
+
+// Send the reply message
+sendFonnte($sender, $reply);
+
+/**
+ * Function to send message using Fonnte API
+ */
 function sendFonnte($target, $data)
 {
     $curl = curl_init();
@@ -32,8 +123,8 @@ function sendFonnte($target, $data)
         CURLOPT_POSTFIELDS => array(
             'target' => $target,
             'message' => $data['message'],
-            'url' => $data['url'],
-            'filename' => $data['filename'],
+            'url' => $data['url'] ?? '',
+            'filename' => $data['filename'] ?? '',
         ),
         CURLOPT_HTTPHEADER => array(
             "Authorization: R1bqBzS9scy-5+uYzRHq"
@@ -41,48 +132,7 @@ function sendFonnte($target, $data)
     ));
 
     $response = curl_exec($curl);
-
     curl_close($curl);
 
     return $response;
 }
-
-if ($message == "test") {
-    $reply = [
-        "message" => "working great!",
-    ];
-} elseif ($message == "image") {
-    $reply = [
-        "message" => "image message",
-        "url" => "https://filesamples.com/samples/image/jpg/sample_640%C3%97426.jpg",
-    ];
-} elseif ($message == "audio") {
-    $reply = [
-        "message" => "audio message",
-        "url" => "https://filesamples.com/samples/audio/mp3/sample3.mp3",
-        "filename" => "music",
-    ];
-} elseif ($message == "video") {
-    $reply = [
-        "message" => "video message",
-        "url" => "https://filesamples.com/samples/video/mp4/sample_640x360.mp4",
-    ];
-} elseif ($message == "file") {
-    $reply = [
-        "message" => "file message",
-        "url" => "https://filesamples.com/samples/document/docx/sample3.docx",
-        "filename" => "document",
-    ];
-} else {
-    $reply = [
-        "message" => "Sorry, i don't understand. Please use one of the following keyword :
-		    
-Test
-Audio
-Video
-Image
-File",
-    ];
-}
-
-sendFonnte($sender, $reply);
